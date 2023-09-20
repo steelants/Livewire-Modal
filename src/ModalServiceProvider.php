c6f2d9dc2af72583f14920ca82edecaa71ff631a
JonatanRek <gamerclass7@gmail.com>
2023-09-20 17:52:27 +0200

    Fix NAme

--- src/ModalServiceProvider.php
+++ src/ModalServiceProvider.php
@@ -11,7 +11,7 @@ class ModalServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        Livewire::component('modal-prefab', ModalPrefab::class);
+        Livewire::component('modal-basic', ModalPrefab::class);
         $this->loadViewsFrom(__DIR__ . '/../resources/views/livewire', 'modal');
         // $this->publishes([
         //     __DIR__ . '/../resources/views/livewire/' => resource_path('views/vendor/modal'),
